4c7a85ca57154ee39f596d16326ee819d916954a
Javier Rosales <javierrosalesdev@gmail.com>
2026-02-09 18:32:57 -0600

    Add rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+/*
+
+Define site property and link to deployed site
+
+export default defineConfig({
+    site: "site-url"
+});
+
+*/
